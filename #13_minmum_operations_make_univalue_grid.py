69c361317f6dc5e424d0aaab8c6c02284c9df92e
yash3028 <134831333+yash3028@users.noreply.github.com>
2026-04-29 16:53:22 +0200

    #13

--- #13_minmum_operations_make_univalue_grid.py
+++ #13_minmum_operations_make_univalue_grid.py
@@ -3,15 +3,21 @@ from typing import List
 
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
+        #convert 2d to 1d array
         one_d = [item for row in grid for item in row]
         rem = one_d[0]%x
+        #check if all elements are different then reutrn -1
         for i in one_d:
             if i%x!=rem:
                 return -1
         one_d.sort()
+        #we will select median value to convert all other elements to its value
+        #why median if two elements on opposite direction of each other the minimum common
+        #point for two of them is median
         median = one_d[len(one_d)//2]
         cnt = 0
         for num in one_d:
+           #formula to find number of times a number needed to add or subtract from a number
            cnt+=abs(num-median)/x
         return int(cnt)
 

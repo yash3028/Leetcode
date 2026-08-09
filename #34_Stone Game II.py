a031935d69ba2f033b3cfdbf05b93767598bce83
yash3028 <134831333+yash3028@users.noreply.github.com>
2026-08-09 22:29:19 +0200

    #34

---
+++ #34_Stone Game II.py
@@ -0,0 +1,29 @@
+from typing import List
+from functools import lru_cache
+
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+        suffix = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
+        @lru_cache(None)
+        def solve(i, M):
+            if i == n:
+                return 0
+
+            best = 0
+
+            for X in range(1, min(2 * M, n - i) + 1):
+                next_M = max(M, X)
+                current = suffix[i] - solve(i + X, next_M)
+                best = max(best, current)
+
+            return best
+
+        return solve(0, 1)
+
+ans = Solution().stoneGameII([2,7,9,4,4])
+print(ans)
